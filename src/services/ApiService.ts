63a26f17d5ab4bbf22ed226697368b38789b5fae
Isaac Hammon <isaac.hammon@nuix.com>
2025-10-07 12:39:43 -0400

    add apiservice with getAsync

---
+++ src/services/ApiService.ts
@@ -0,0 +1,14 @@
+const getAsync = async (url: string): Promise<Response> => {
+  const response = await fetch(url, {
+    method: "GET",
+  });
+
+  return await response.json();
+};
+// const postASync = async (url: string, body: unknown): Promise<Response> => {
+//     const response = await fetch(url, {
+//         method: 'POST',
+//         data: JSON.stringify(body),
+//     });
+
+export { getAsync };
